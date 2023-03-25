7a5ada96085d175a68a153069611289dcd449286
molitoris <rafa.molitoris@gmail.com>
2023-03-25 10:04:34 +0000

    Add hardware library

--- drivers/touch/touch.cmake
+++ drivers/touch/touch.cmake
@@ -8,4 +8,4 @@ target_sources(${DRIVER_NAME} INTERFACE
 target_include_directories(${DRIVER_NAME} INTERFACE ${CMAKE_CURRENT_LIST_DIR}/include)
 
 # Pull in pico libraries that we need
-target_link_libraries(${DRIVER_NAME} INTERFACE pico_stdlib)
+target_link_libraries(${DRIVER_NAME} INTERFACE pico_stdlib hardware_i2c)
